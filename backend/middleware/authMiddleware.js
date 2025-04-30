fad10dce14d494f88a6742ba489943f53150a768
Kaleem736-bit <188190993+Kaleem736-bit@users.noreply.github.com>
2025-04-30 04:12:06 +0300

    Create authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(403).json({ message: 'No token' });
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, 'secretKey');
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(403).json({ message: 'Invalid token' });
+  }
+}
+
+module.exports = authMiddleware;
